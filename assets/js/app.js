a4b6b6cbf0fccd9529f971c7e2aac64986a64364
devluizmarcolino <devluizmarcolino@gmail.com>
2024-09-24 22:34:08 -0300

    seção home concluída | adicionado algumas animações

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,42 @@
-let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('btn-dark-mode-toogle')
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("btn-dark-mode-toogle");
 
 const enableDarkmode = () => {
-  document.body.classList.add('darkmode')
-  localStorage.setItem('darkmode', 'active')
-}
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
 
 const disableDarkmode = () => {
-  document.body.classList.remove('darkmode')
-  localStorage.setItem('darkmode', null)
-}
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
 
-if(darkmode === "active") enableDarkmode()
+if (darkmode === "active") enableDarkmode();
 
 themeSwitch.addEventListener("click", () => {
-  darkmode = localStorage.getItem('darkmode')
-  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-})
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
 
-function showSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'flex'
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
 }
 
-function hideSidebar(){
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'none'
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
 }
+
+window.gtranslateSettings = {
+  default_language: "pt",
+  detect_browser_language: true,
+  languages: ["pt", "en"],
+  wrapper_selector: ".gtranslate_wrapper",
+  switcher_horizontal_position: "right",
+  float_switcher_open_direction: "right",
+  flag_style: "3d",
+  alt_flags: { en: "usa", pt: "brazil" },
+};
+
+AOS.init();
